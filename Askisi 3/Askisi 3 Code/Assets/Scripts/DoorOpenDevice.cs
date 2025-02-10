e454d5b71ecf01ea41bf8c2f5dbd4471b692891d
Orestis Makris <79021400+OrestisMakris@users.noreply.github.com>
2025-02-10 21:59:56 +0200

    door problems

--- Askisi 3/Askisi 3 Code/Assets/Scripts/DoorOpenDevice.cs
+++ Askisi 3/Askisi 3 Code/Assets/Scripts/DoorOpenDevice.cs
@@ -1,58 +1,46 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening; // Import DOTween namespace
 
 public class DoorOpenDevice : MonoBehaviour
 {
-    [SerializeField] Vector3 dPos;
+    [SerializeField] private Vector3 dPos;
     private bool open;
+    [SerializeField] private float duration = 1f; // Duration of the tween
 
+    // Operates the door based on its current state
     public void Operate()
     {
-        if (open)
-        {
-            Vector3 pos = transform.position - dPos;
-            transform.position = pos;
-        }
-
-        else
-        {
-            Vector3 pos = transform.position + dPos;
-            transform.position = pos;
-        }
-
-        open = !open;
+        Vector3 targetPos = open ? transform.position - dPos : transform.position + dPos;
+        transform.DOMove(targetPos, duration).OnComplete(() => open = !open);
     }
 
+    // Activates the door to open position
     public void Activate()
     {
         if (!open)
         {
-            Vector3 pos = transform.position + dPos;
-            transform.position = pos;
-            open = true;
+            transform.DOMove(transform.position + dPos, duration).OnComplete(() => open = true);
         }
     }
 
+    // Deactivates the door to closed position
     public void Deactivate()
     {
         if (open)
         {
-            Vector3 pos = transform.position - dPos;
-            transform.position = pos;
-            open = false;
+            transform.DOMove(transform.position - dPos, duration).OnComplete(() => open = false);
         }
     }
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-
+        // Initialization code if needed
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-
+        // Per-frame update logic if needed
     }
 }
